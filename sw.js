f062b433c6bec4566e2b41bd66ebe2f45e886eed
Asus <dialprinter69@gmail.com>
2026-04-20 07:32:27 -0400

    Increase touch target size and spacing from delivery section down

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "delicias-pwa-v20";
+const CACHE = "delicias-pwa-v21";
 const PRECACHE = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icons/icon.svg"];
 
 self.addEventListener("install", (event) => {
